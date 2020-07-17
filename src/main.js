15e58a490c9039010eb943b44815ccada8742ec1
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-07-17 14:31:44 +0900

    Add main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let pages = ['sample', '10sChallenge', 'sigmabeat', 'tetTyping'];  // ToDo : 動的に、自動で page ディレクトリより取得
+let pageFiles = new Object();
+document.addEventListener('DOMContentLoaded', async () => {
+    
+    for (let i = 0; i < pages.length; i++) {
+        pageFiles[i] = (await (getJSON(pages[i] + '.json')));
+    } // ToDo 上限を設けて、More ボタンでさらに読み込む
+    console.log(pageFiles[0]);
+})
+
+async function getJSON(fileName) {
+    let jsonContent;
+    await fetch(fileName).then(r => r.text()).then(t => { jsonContent = JSON.parse(t); });
+    return jsonContent;
+}
